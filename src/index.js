ea9f4a3846d5f16b8f842da5c0171bce942456ec
AdarshPattnaik <adarshme05102002@gmail.com>
2023-08-01 00:20:23 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
+// Main React File: index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Importing Bootstrap:
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+
+// Importing CSS File:
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Importing App Component:
+import App from "./Components/App.jsx";
+
+// Importing BrowserRouter:
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
